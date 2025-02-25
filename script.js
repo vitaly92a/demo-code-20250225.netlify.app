d994a59b61351a4e846a6f3f313f4d891c14a409
vitaly92a <vitaly92a@gmail.com>
2025-02-25 14:36:19 +0300

    website 20250225 init
    javascript reserved words website init

---
+++ script.js
@@ -0,0 +1,5 @@
+/* Source: https://www.w3schools.com/js/js_reserved.asp */
+"use strict";
+
+// Set the font size of the body element using setProperty
+document.body.style.setProperty("font-size", "16px");
